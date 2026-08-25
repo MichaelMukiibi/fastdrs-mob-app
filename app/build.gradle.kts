0b3a9f0a9ac29f0d57d1dd4465f90775f51d35f6
Michael Mukiibi <michael.mukiibi.k@gmail.com>
2026-08-25 16:48:27 +0300

    fix: changed minsdk to 33

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,16 +5,12 @@ plugins {
 
 android {
     namespace = "com.campmap.fastdrs"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.campmap.fastdrs"
-        minSdk = 34
-        targetSdk = 36
+        minSdk = 33
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
