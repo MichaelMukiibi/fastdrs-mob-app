b96859d38c1b6f96e1147e1e17962d1adcc660a9
Michael Mukiibi <michael.mukiibi.k@gmail.com>
2026-08-28 11:56:19 +0300

    feat: implement screening setup and viewmodel

--- app/src/main/java/com/campmap/fastdrs/navigation/AppNavHost.kt
+++ app/src/main/java/com/campmap/fastdrs/navigation/AppNavHost.kt
@@ -4,19 +4,28 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.campmap.fastdrs.feature.home.HomeScreen
+import com.campmap.fastdrs.feature.screening.ScreeningViewModel
+import com.campmap.fastdrs.feature.screening.setup.ScreeningSetupScreen
 
 @Composable
 fun AppNavHost() {
     val navController = rememberNavController()
+    val viewModel: ScreeningViewModel = viewModel()
     NavHost(navController = navController, startDestination = Screen.Home.route) {
         composable(Screen.Home.route) {
             HomeScreen(onStartScreening = {
                 navController.navigate(Screen.ScreeningSetup.route)
             })
         }
-        // Placeholder screens for flow completion
-        composable(Screen.ScreeningSetup.route) { /* TODO */ }
+        composable(Screen.ScreeningSetup.route) {
+            ScreeningSetupScreen(onContinue = { eye ->
+                viewModel.startScreening(eye)
+                navController.navigate(Screen.ImageAcquisition.route)
+            })
+        }
         composable(Screen.ImageAcquisition.route) { /* TODO */ }
         composable(Screen.ImageReview.route) { /* TODO */ }
         composable(Screen.Analysis.route) { /* TODO */ }
